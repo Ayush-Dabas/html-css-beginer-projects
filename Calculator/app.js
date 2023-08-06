5522ad2c752b500833862c34b8dfe2d56dbe9854
Ayush-Dabas <aydbs23@gmail.com>
2023-08-06 10:47:03 +0530

    calculator made with no css

---
+++ Calculator/app.js
@@ -0,0 +1,11 @@
+const displayString = document.getElementById("dispRes");
+
+// input function to take the input from the user
+function takeInput() {
+  let num1 = document.getElementById("1");
+}
+
+// calculate function for calculating the result
+
+// display change function
+function displayChange(result, prevResult) {}
